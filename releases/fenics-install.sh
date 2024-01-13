dd10929f66b492106d9216174ba3a64f0df1a01b
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-01-13 07:15:58 +0100

    Warn users who are trying to download FEniCS and NGSolve from legacy URLs

--- releases/fenics-install.sh
+++ releases/fenics-install.sh
@@ -1,56 +1,12 @@
-# Copyright (C) 2021-2023 by the FEM on Colab authors
+# Copyright (C) 2021-2024 by the FEM on Colab authors
 #
 # This file is part of FEM on Colab.
 #
 # SPDX-License-Identifier: MIT
 
-set -e
-set -x
+# Use a mock package to keep track of how many users are still trying to download FEniCS from the legacy URL
+wget -O/dev/null -q https://github.com/fem-on-kaggle/fem-on-kaggle/releases/download/mock-20240111-070849-9b9a7aa/mock-install.tar.gz
 
-# Check for existing installation
-INSTALL_PREFIX=${INSTALL_PREFIX:-"/root/.local"}
-INSTALL_PREFIX_DEPTH=$(echo $INSTALL_PREFIX | awk -F"/" '{print NF-1}')
-PROJECT_NAME=${PROJECT_NAME:-"fem-on-kaggle"}
-SHARE_PREFIX="$INSTALL_PREFIX/share/$PROJECT_NAME"
-FENICS_INSTALLED="$SHARE_PREFIX/fenics.installed"
-
-if [[ ! -f $FENICS_INSTALLED ]]; then
-    # Install pybind11
-    PYBIND11_INSTALL_SCRIPT_PATH=${PYBIND11_INSTALL_SCRIPT_PATH:-"https://github.com/fem-on-kaggle/fem-on-kaggle.github.io/raw/351f9a9/releases/pybind11-install.sh"}
-    [[ $PYBIND11_INSTALL_SCRIPT_PATH == http* ]] && PYBIND11_INSTALL_SCRIPT_DOWNLOAD=${PYBIND11_INSTALL_SCRIPT_PATH} && PYBIND11_INSTALL_SCRIPT_PATH=/tmp/pybind11-install.sh && [[ ! -f ${PYBIND11_INSTALL_SCRIPT_PATH} ]] && wget ${PYBIND11_INSTALL_SCRIPT_DOWNLOAD} -O ${PYBIND11_INSTALL_SCRIPT_PATH}
-    source $PYBIND11_INSTALL_SCRIPT_PATH
-
-    # Install boost (and its dependencies)
-    BOOST_INSTALL_SCRIPT_PATH=${BOOST_INSTALL_SCRIPT_PATH:-"https://github.com/fem-on-kaggle/fem-on-kaggle.github.io/raw/611ea33/releases/boost-install.sh"}
-    [[ $BOOST_INSTALL_SCRIPT_PATH == http* ]] && BOOST_INSTALL_SCRIPT_DOWNLOAD=${BOOST_INSTALL_SCRIPT_PATH} && BOOST_INSTALL_SCRIPT_PATH=/tmp/boost-install.sh && [[ ! -f ${BOOST_INSTALL_SCRIPT_PATH} ]] && wget ${BOOST_INSTALL_SCRIPT_DOWNLOAD} -O ${BOOST_INSTALL_SCRIPT_PATH}
-    source $BOOST_INSTALL_SCRIPT_PATH
-
-    # Install slepc4py (and its dependencies)
-    SLEPC4PY_INSTALL_SCRIPT_PATH=${SLEPC4PY_INSTALL_SCRIPT_PATH:-"https://github.com/fem-on-kaggle/fem-on-kaggle.github.io/raw/9858ef6/releases/slepc4py-install-real.sh"}
-    [[ $SLEPC4PY_INSTALL_SCRIPT_PATH == http* ]] && SLEPC4PY_INSTALL_SCRIPT_DOWNLOAD=${SLEPC4PY_INSTALL_SCRIPT_PATH} && SLEPC4PY_INSTALL_SCRIPT_PATH=/tmp/slepc4py-install.sh && [[ ! -f ${SLEPC4PY_INSTALL_SCRIPT_PATH} ]] && wget ${SLEPC4PY_INSTALL_SCRIPT_DOWNLOAD} -O ${SLEPC4PY_INSTALL_SCRIPT_PATH}
-    source $SLEPC4PY_INSTALL_SCRIPT_PATH
-
-    # Download and uncompress library archive
-    FENICS_ARCHIVE_PATH=${FENICS_ARCHIVE_PATH:-"https://github.com/fem-on-kaggle/fem-on-kaggle/releases/download/fenics-20231113-115636-88f5001/fenics-install.tar.gz"}
-    [[ $FENICS_ARCHIVE_PATH == http* ]] && FENICS_ARCHIVE_DOWNLOAD=${FENICS_ARCHIVE_PATH} && FENICS_ARCHIVE_PATH=/tmp/fenics-install.tar.gz && wget ${FENICS_ARCHIVE_DOWNLOAD} -O ${FENICS_ARCHIVE_PATH}
-    if [[ $FENICS_ARCHIVE_PATH != skip ]]; then
-        tar -xzf $FENICS_ARCHIVE_PATH --strip-components=$INSTALL_PREFIX_DEPTH --directory=$INSTALL_PREFIX
-    fi
-
-    # Add symbolic links to FEniCS libraries in /usr/lib, because INSTALL_PREFIX/lib may not be in LD_LIBRARY_PATH
-    # on the actual cloud instance
-    if [[ $FENICS_ARCHIVE_PATH != skip ]]; then
-        ln -fs $INSTALL_PREFIX/lib/libdolfin*.so* /usr/lib
-        ln -fs $INSTALL_PREFIX/lib/libmshr*.so* /usr/lib
-    fi
-
-    # Mark package as installed
-    mkdir -p $SHARE_PREFIX
-    touch $FENICS_INSTALLED
-fi
-
-# Display end user packages announcement
-set +x
 cat << EOF
 
 
@@ -76,18 +32,26 @@ cat << EOF
 
 
 
+
+
+
+
 ################################################################################
-#    This installation is offered by FEM on Kaggle, an open-source project     #
-#       developed and maintained at Università Cattolica del Sacro Cuore       #
-#    by Dr. Francesco Ballarin. Please see https://fem-on-kaggle.github.io/    #
-#       for more details, including a list of further available packages       #
-#       and how to sponsor the development or contribute to the project.       #
 #                                                                              #
-#  We are conducting an informal survey on FEM on Kaggle usage by our users.   #
-#   The survey is anonymous, and its compilation will typically only require   #
-#   a couple of minutes of your time. If you wish, give us your feedback at    #
-#                     https://forms.gle/36sZZWNvPpUv8XWr7                      #
+#         The installation script for legacy FEniCS has been moved from        #
+#          https://fem-on-kaggle.github.io/releases/fenics-install.sh          #
+#                                      to                                      #
+#       https://fem-on-kaggle.github.io/releases/fenics-install-real.sh        #
+#                   (note the additional "-real" in the URL).                  #
+#                                                                              #
+#   Please update your installation cell. Report issues at our issue tracker   #
+#           https://github.com/fem-on-kaggle/fem-on-kaggle/issues              #
+#                                                                              #
 ################################################################################
+
+
+
+
 
 
 
@@ -113,4 +77,4 @@ cat << EOF
 
 
 EOF
-set -x
+sleep 2; kill -9 `ps --pid $$ -oppid=`; exit
